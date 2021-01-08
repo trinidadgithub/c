431bc9c8b0f520fce020dd0451b7805128119c82
trinidadgithub <trinidad.marroquin@gmail.com>
2021-01-08 16:36:13 -0600

    Updated code

--- charExample2.c
+++ charExample2.c
@@ -1,20 +1,16 @@
 /* Filename: charExample2.c
  * The program domonstrates printing extended ASCII character */
 #include <stdio.h>
-#include <locale.h>
 
 unsigned char x; /* Must be unsigned for extended ASCII */
 
 main()
 {
-	/* Print extended ASCII characters 180 through 203 */
-	setlocale(LC_CTYPE, "");
-	printf("ASCII code 255 is character %c\n", 255 );
-	
-	/* for ( x = 1; x < 255; x++ )
+	for ( x = 0; x < 255; x++ )
 	{
 		printf("ASCII code %d is character %c\n", x, x );
 	}
-	return 0; */
+	printf("ASCII code %d is character %c\n", 255, 255 );
+	return 0; 
 }
 
