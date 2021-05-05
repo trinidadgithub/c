a2e59e88ae0e54723690cf7a741def5ca5bd9b97
Trinidad Marroquin <Trinidad.Marroquin@argogroupus.com>
2021-05-05 10:24:04 -0500

    commiting latest c program

--- charExample2.c
+++ charExample2.c
@@ -1,16 +1,18 @@
 /* Filename: charExample2.c
- * The program domonstrates printing extended ASCII character */
+ * The program domonstrates printing extended ASCII character,
+ * your terminal should be set to allow the printing of extended
+ * characters, i.e. set to ISO-8859-15 or CP437 */
 #include <stdio.h>
 
 unsigned char x; /* Must be unsigned for extended ASCII */
 
 main()
 {
-	for ( x = 0; x < 255; x++ )
+	/* Print extended ASCII characters 180 through 203 */
+	for ( x = 180; x < 204; x++ )
 	{
 		printf("ASCII code %d is character %c\n", x, x );
 	}
-	printf("ASCII code %d is character %c\n", 255, 255 );
 	return 0; 
 }
 
